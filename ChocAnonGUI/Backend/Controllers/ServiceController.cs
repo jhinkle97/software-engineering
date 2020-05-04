dac86310405a1fceb8cc72715e2b90a885539835
Justin <justinrh97@gmail.com>
2020-05-03 20:42:28 -0400

    AddService and GetServicesByMember are now working. When looking up a service you can click on the code to fill in the text box.

--- ChocAnonGUI/Backend/Controllers/ServiceController.cs
+++ ChocAnonGUI/Backend/Controllers/ServiceController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ChocAnonGUI.Backend.DataAccess;
+using ChocAnonGUI.Backend.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,29 @@ namespace ChocAnonGUI.Backend.Controllers
 {
     class ServiceController
     {
+        public bool AddService(ServiceModel service)
+        {
+            ServiceRepository serviceRepository = new ServiceRepository();
+            service.EntryDate = DateTime.Now;
+            return serviceRepository.AddService(service);
+        }
+
+        public IEnumerable<ServiceModel> GetServicesByMember(string memberNumber)
+        {
+            ServiceRepository serviceRepository = new ServiceRepository();
+            UserRepository userRepository = new UserRepository();
+            ServiceDirectoryRepository serviceDirectoryRepository = new ServiceDirectoryRepository();
+
+            IEnumerable<ServiceModel> services = serviceRepository.GetServicesByMember(memberNumber);
+            UserModel member = userRepository.GetUser(memberNumber);
+
+            foreach(ServiceModel service in services)
+            {
+                service.Member = member;
+                service.Provider = userRepository.GetUser(service.Provider.UserNumber);
+                service.ServiceDirectory = serviceDirectoryRepository.GetService(service.ServiceDirectory.Code);
+            }
+            return services;
+        }
     }
 }
--- ChocAnonGUI/Backend/DataAccess/ServiceRepository.cs
+++ ChocAnonGUI/Backend/DataAccess/ServiceRepository.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using ChocAnonGUI.Backend.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +11,66 @@ namespace ChocAnonGUI.Backend.DataAccess
 {
     class ServiceRepository
     {
+        private readonly SqlConnection connection = new SqlConnection(@"Data Source=.; Initial Catalog=chocAnon; Integrated Security=SSPI");
+
+        public bool AddService(ServiceModel service)
+        {
+            string query = $"INSERT INTO [service] VALUES('{service.EntryDate}', '{service.ServiceDate}', '{service.Provider.UserNumber}', " +
+                                                        $"'{service.Member.UserNumber}', '{service.ServiceDirectory.Code}', '{service.Comments}')";
+
+            try
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                var result = cmd.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+                connection.Close();
+                return false;
+            }
+            return true;
+        }
+
+        public IEnumerable<ServiceModel> GetServicesByMember(string memberNumber)
+        {
+            try
+            {
+                string query = $"SELECT * FROM [service] WHERE [memberNumber] = '{memberNumber}'";
+
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                List<ServiceModel> services = new List<ServiceModel>();
+
+                while (reader.Read())
+                {
+                    IDataRecord record = reader;
+
+                    ServiceModel service = new ServiceModel
+                    {
+                        EntryDate = (DateTime)record[1],
+                        ServiceDate = (DateTime)record[2],
+                        Provider = new UserModel { UserNumber=(string)record[3] },
+                        Member = new UserModel { UserNumber=(string)record[4] },
+                        ServiceDirectory = new ServiceDirectoryModel { Code=(string)record[5] },
+                        Comments = (string)record[6]
+                    };
+                    services.Add(service);
+                }
+                connection.Close();
+                return services;
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+                connection.Close();
+                return new List<ServiceModel>();
+            }
+        }
     }
 }
