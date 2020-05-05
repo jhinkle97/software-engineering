ad5d6c747347723d59f39276429ce0a3fb0787b4
Justin <justinrh97@gmail.com>
2020-05-04 22:40:10 -0400

    Write service_directory to file whenever data is updated.

--- ChocAnonGUI/Backend/Utilities/FileHandler.cs
+++ ChocAnonGUI/Backend/Utilities/FileHandler.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChocAnonGUI.Backend.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -37,6 +38,33 @@ namespace ChocAnonGUI.Backend.Utilities
             StreamWriter file = new StreamWriter($"{path}\\{fileName}.txt");
             {
                 file.WriteLine(report);
+            }
+            file.Close();
+        }
+
+        public static void WriteServiceDirectory()
+        {
+            string path = $"{desktopPath}\\{serviceDirectoryPath}";
+
+            Directory.CreateDirectory(path);
+
+            var serviceDirectoryRepository = new ServiceDirectoryRepository();
+
+            var services = serviceDirectoryRepository.GetServices();
+
+            string service_directory = "SERVICE DIRECTORY\n\n";
+            foreach (var service in services)
+            {
+                service_directory +=    $"Service Name: {service.Name}\n" +
+                                        $"Service Code: {service.Code}\n" +
+                                        $"Service Fee:  {service.Fee}\n" +
+                                        $"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n";
+            }
+
+            StreamWriter file = new StreamWriter($"{path}\\service_directory.txt");
+            {
+                
+                file.WriteLine(service_directory);
             }
             file.Close();
         }
