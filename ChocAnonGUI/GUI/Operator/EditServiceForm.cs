189b9f3b4864c518a0d9587dcf2412dd240ce695
Justin <justinrh97@gmail.com>
2020-05-06 12:56:33 -0400

    Update edit service and edit user

--- ChocAnonGUI/GUI/Operator/EditServiceForm.cs
+++ ChocAnonGUI/GUI/Operator/EditServiceForm.cs
@@ -29,83 +29,76 @@ namespace ChocAnonGUI
 
         private void editButton_Click(object sender, EventArgs e)
         {
-            if (enableButtons == false)
-            {
-                PopupControl.printError("Sorry, No valid \n\n service ID entered");
-                lockFields();
+            if (serviceTextbox.Text == "" || feeTextbox.Text == "")
+              {
+                  PopupControl.printError("Fill in the empty fields please");
             }
-            else if (enableButtons == true)
+            else
             {
-              if (serviceTextbox.Text == "" || feeTextbox.Text == "")
+                ServiceDirectoryController editService = new ServiceDirectoryController();
+                service.Name = serviceTextbox.Text;
+                bool validFee = true;
+
+                //TRY CATCH
+                try
                 {
-                    PopupControl.printError("Fill in the empty fields please");
+                    service.Fee = Convert.ToDecimal(feeTextbox.Text, new CultureInfo("en-US"));
+                }
+                catch
+                {
+                    validFee = false;
+                    service.Fee = 0;
+                }
+                if (!validFee || service.Fee > (decimal)999.99)
+                {
+                    PopupControl.printError("Invalid fee format.\n\nPlease enter as a number or decimal\nwith a value less than 1000.00");
                 }
                 else
                 {
-                    ServiceDirectoryController editService = new ServiceDirectoryController();
-                    service.Name = serviceTextbox.Text;
-                    bool validFee = true;
-
-                    //TRY CATCH
-                    try
+                    service = editService.EditService(service);
+                    if (service.Code == null)
                     {
-                        service.Fee = Convert.ToDecimal(feeTextbox.Text, new CultureInfo("en-US"));
-                    }
-                    catch
-                    {
-                        validFee = false;
-                        service.Fee = 0;
-                    }
-                    if (!validFee || service.Fee > (decimal)999.99)
-                    {
-                        PopupControl.printError("Invalid fee format.\n\nPlease enter as a number or decimal\nwith a value less than 1000.00");
+                        PopupControl.printError("Something went wrong.");
                     }
                     else
                     {
-                        service = editService.EditService(service);
                         ServiceEntryConfirmationForm launchConfirmation = new ServiceEntryConfirmationForm(service);
                         launchConfirmation.ShowDialog();
-                        this.Close();
                     }
+                    this.Close();
                 }
-
             }
         }
 
         private void deleteButton_Click(object sender, EventArgs e)
         {
-            if (enableButtons == false)
+            ServiceDirectoryController deleteService = new ServiceDirectoryController();
+            if (deleteService.DeleteService(service.Code))
             {
-                PopupControl.printError("Sorry, No valid \n\n service ID entered");
-                lockFields();
-            }
-            else if (enableButtons == true)
-            {
-                ServiceDirectoryController deleteService = new ServiceDirectoryController();
-                deleteService.DeleteService(service.Code);
                 PopupControl.printSuccess("The Service with Code: " + service.Code + "\n\n was removed");
-                this.Close();
             }
+            else
+            {
+                PopupControl.printError("Something went wrong.");
+            }
+            this.Close();
         }
 
         private void searchButton_Click(object sender, EventArgs e)
         {
-
             string serviceCode = inputTextbox.Text;
 
             ServiceDirectoryController serviceDirectoryController = new ServiceDirectoryController();
             service = serviceDirectoryController.GetService(serviceCode);
 
-            if (service.Code == "")
+            if (service.Code == null)
             {
                 lockFields();
-                enableButtons = false;
                 PopupControl.printError("Not a valid \n\n service code...");
             }
             else
             {
                 unlockFields();
-                enableButtons = true;
                 serviceTextbox.Text = service.Name;
                 feeTextbox.Text = service.Fee.ToString();
             }
@@ -113,13 +106,17 @@ namespace ChocAnonGUI
 
         private void lockFields()
         {
-            serviceTextbox.ReadOnly = true;
-            feeTextbox.ReadOnly = true;
+            serviceTextbox.Enabled = false;
+            feeTextbox.Enabled = false;
+            editButton.Enabled = false;
+            deleteButton.Enabled = false;
         }
         private void unlockFields()
         {
-            serviceTextbox.ReadOnly = false;
-            feeTextbox.ReadOnly = false;
+            serviceTextbox.Enabled = true;
+            feeTextbox.Enabled = true;
+            editButton.Enabled = true;
+            deleteButton.Enabled = true;
         }
     }
 }
