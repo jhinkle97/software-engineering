1f55d85c27808ac3caeaa5809fe7fb77edf4b9c0
Riley Kalb <rkalb@rockets.utoledo.edu>
2020-05-05 21:17:03 -0500

    Billing Popup now works. May want more fields

--- ChocAnonGUI/GUI/Provider/Billing/ConfirmServiceForm.cs
+++ ChocAnonGUI/GUI/Provider/Billing/ConfirmServiceForm.cs
@@ -7,19 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ChocAnonGUI.Backend.Controllers;
+using ChocAnonGUI.Backend.Models;
+
+
 
 namespace ChocAnonGUI
 {
     public partial class ConfirmServiceForm : Form
     {
-        public ConfirmServiceForm()
+        private ServiceModel billedService;
+        public ConfirmServiceForm(ServiceModel service)
         {
             InitializeComponent();
+            billedService = service;
+            providerLabel.Text = billedService.Provider.Name;
+            serviceLabel.Text = billedService.ServiceDirectory.Code;
         }
 
         private void confirmButton_Click(object sender, EventArgs e)
         {
-            this.Close();
+            if (codeTextbox.Text == billedService.ServiceDirectory.Code)
+            {
+                ServiceController serviceController = new ServiceController();
+                serviceController.AddService(billedService);
+                PopupControl.printSuccess("Billing Submission Complete");
+                this.Close();
+            }
+            else
+            {
+                PopupControl.printError("Sorry, given code did not \n\n match the entered code");
+            }
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
